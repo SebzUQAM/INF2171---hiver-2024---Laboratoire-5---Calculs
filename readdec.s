fa778055c75600149a80ab09c84a0278ed51d40e
Sebastien Marcil <sebz.marcil@gmail.com>
2024-02-06 00:37:45 -0500

    Ex 01 négatif

--- readdec.s
+++ readdec.s
@@ -1,12 +1,24 @@
 	.eqv readchar, 12
+	.eqv printchar, 11
 	.eqv printint, 1
 	.eqv exit, 10
 	
 	li s0, 0
+	li s2, 0
 	
 loop:
 	li a7, readchar
 	ecall
+	
+	li t0, 0
+	bne s2, t0, notfirst
+		li t0, '-'
+		bne a0, t0, positif
+		li s2, -1
+		j loop
+		positif :
+		li s2, 1
+	notfirst :
 	
 	li t0, '0'
 	blt a0, t0, end
@@ -21,6 +33,12 @@ loop:
 	j loop
 	
 end :
+	li a7, printchar
+	li a0, '\n'
+	ecall
+	
+	mul s0, s0, s2
+
 	li a7, printint
 	mv a0, s0
 	ecall
